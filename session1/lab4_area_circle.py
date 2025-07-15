fe440266c7c608463da74536f2f2abec39dea39c
Menna-Ahmed <menna0969@gmail.com>
2025-07-15 03:23:32 +0300

    Completed my assignment

--- session1/lab4_area_circle.py
+++ session1/lab4_area_circle.py
@@ -7,6 +7,7 @@ def area(number1):
     and compute the area. Don't forget to return the result at the end.
     """
     return math.pi*number1**2
+
 if __name__ == "__main__":
     assert area(2) == 12.566370614359172, "Test case failed"
     assert area(1) == 3.141592653589793, "Test case failed"
